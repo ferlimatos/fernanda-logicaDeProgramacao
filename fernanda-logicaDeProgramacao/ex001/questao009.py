d4661384c91a647c2862b70fdee33f3d8a1c70e2
ferlimatos <fernandamatos.design@gmail.com>
2025-11-20 12:08:11 -0300

    corrigindo questao 9

--- fernanda-logicaDeProgramacao/ex001/questao009.py
+++ fernanda-logicaDeProgramacao/ex001/questao009.py
@@ -2,20 +2,23 @@
 
 compra_valor = float(input('Informe o valor de sua compra: R$'))
 
-if compra_valor >= 500:
-    print(f'Parabens, você ganhou um frete grátis e um brinde!')
+if compra_valor >= 200:
+    if compra_valor >= 500:
+        print(f'Parabéns, você ganhou um frete grátis e um brinde!')
+    else:
+        print('Parabéns, você ganhou um frete grátis.')
     frete = 0
+    
 else:
-    print('Parabéns, você ganhou um frete grátis.')
+    if compra_valor >50:
+        frete = 10
+
+    else:
+        frete = 20
 
 valor_total = compra_valor + frete
 
-if compra_valor <= 50:
-    if frete == 20:
-        print(f'O valor do frete é de R${frete}. Portanto, o valor total que precisa ser pago é R${valor_total}')
-else:
-    frete > 10
-    print(f'O valor do frete é de R${frete}. Portanto, o valor total que precisa ser pago é R${valor_total}')
+print(f'O valor do frete é de R${frete}. Portanto, o valor total que precisa ser pago é R${valor_total}')
 
 
 
