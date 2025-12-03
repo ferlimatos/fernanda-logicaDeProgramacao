9ff097c08ba83d3f9ffa6137a2763a452d321d3a
Fernanda Matos <101283069+ferlimatos@users.noreply.github.com>
2025-12-03 19:26:14 -0300

    Update ex002.py
    Refinando a resposta do exercício.

--- fernanda-logicaDeProgramacao/aula-7/listas-exercícios/ex002.py
+++ fernanda-logicaDeProgramacao/aula-7/listas-exercícios/ex002.py
@@ -8,15 +8,15 @@
 numeros = [5, 10, 20, 50, 100]
 
 maior_numero = max(numeros)
-print(f"a. O maior número é: {maior_numero}")
+print(f"a. O maior número da lista é: {maior_numero}")
 
 menor_numero = min(numeros)
-print(f"b. O menor número é: {menor_numero}")
+print(f"b. O menor número da lista é: {menor_numero}")
 
 soma_dos_numeros = sum(numeros)
-print(f"c. A soma dos números é: {soma_dos_numeros}")
+print(f"c. A soma dos números da lista é: {soma_dos_numeros}")
 
 quantidade = len(numeros)
 media = soma_dos_numeros / quantidade
 
-print(f"d. A média dos números é: {media:.2f}")
+print(f"d. A média dos números da lista é: {media:.2f}")
