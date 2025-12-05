1ff4348b47ebe2e541c1c095bf6c0f980d59c024
ferlimatos <fernandamatos.design@gmail.com>
2025-12-05 17:44:43 -0300

    adicionando sistemas de notas escolares

--- projetos-práticos/sistemas-notas-escolares.py
+++ projetos-práticos/sistemas-notas-escolares.py
@@ -1,1 +1,15 @@
 # Sistema de notas escolares: receba a média do aluno e informe se foi aprovado, reprovado ou em recuperação.
+
+# Média do aluno (float)
+media = float(input("Informe a sua média: "))
+
+# Se a média >= 7: Aprovado
+# Senão, média >= 5: Recuperação
+# Senão: Reprovado
+
+if media >= 7:
+  print("Parabéns, você foi aprovado!")
+elif media >= 5:
+  print("Você está de recuperação.")
+else:
+  print("Você está reprovado!")
